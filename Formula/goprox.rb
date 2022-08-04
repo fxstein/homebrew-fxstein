882847bfa96a3a4618a17cc0d4e468f136199cf1
fxstein <fxstein@users.noreply.github.com>
2022-08-03 23:50:20 -0700

    Cleanup homebrew style errors

--- Formula/goprox.rb
+++ Formula/goprox.rb
@@ -1,15 +1,15 @@
 class Goprox < Formula
-    desc "GoProX - The missing GoPro workflow manager for zsh."
-    homepage "https://github.com/fxstein/GoProX"
-    url "https://github.com/fxstein/GoProX/archive/refs/tags/v00.08.08.tar.gz"
-    sha256 "c104ef3d0efa1006213e1fea6333004a16cad1d9ca6dbcd5dcbaeafe6fb04ea0"
-    license "MIT"
+  desc "The missing GoPro workflow manager for zsh"
+  homepage "https://github.com/fxstein/GoProX"
+  url "https://github.com/fxstein/GoProX/archive/refs/tags/v00.08.08.tar.gz"
+  sha256 "c104ef3d0efa1006213e1fea6333004a16cad1d9ca6dbcd5dcbaeafe6fb04ea0"
+  license "MIT"
   
-    depends_on "exiftool"
-    depends_on "jq"
-    depends_on "tag"
+  depends_on "exiftool"
+  depends_on "jq"
+  depends_on "tag"
   
-    def install
-      bin.install 'goprox'
-    end
+  def install
+    bin.install "goprox"
   end
+end
