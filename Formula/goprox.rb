7b940123c63bea40abebed02426ba1aa96fb6571
fxstein <fxstein@users.noreply.github.com>
2022-08-03 23:52:43 -0700

    More style formatting fixes

--- Formula/goprox.rb
+++ Formula/goprox.rb
@@ -1,14 +1,14 @@
 class Goprox < Formula
-  desc "The missing GoPro workflow manager for zsh"
+  desc "Missing GoPro workflow manager for zsh"
   homepage "https://github.com/fxstein/GoProX"
   url "https://github.com/fxstein/GoProX/archive/refs/tags/v00.08.08.tar.gz"
   sha256 "c104ef3d0efa1006213e1fea6333004a16cad1d9ca6dbcd5dcbaeafe6fb04ea0"
   license "MIT"
-  
+
   depends_on "exiftool"
   depends_on "jq"
   depends_on "tag"
-  
+
   def install
     bin.install "goprox"
   end
