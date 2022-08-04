6bf3cb4a4e47a8ae04f951a13c43598449a6f537
fxstein <fxstein@users.noreply.github.com>
2022-08-03 19:46:24 -0700

    Update goprox.rb

--- Formula/goprox.rb
+++ Formula/goprox.rb
@@ -5,7 +5,7 @@ class Goprox < Formula
     sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
     license "MIT"
   
-    depends_on "exiftool"
+    # depends_on "exiftool"
     depends_on "jq"
     depends_on "tag"
   
