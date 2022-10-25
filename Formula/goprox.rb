ee8b1cdd8d619bf577eb781cc84bcf7a461c76c8
fxstein <fxstein@users.noreply.github.com>
2022-10-24 22:49:10 -0700

    Update goprox to v00.16.00

--- Formula/goprox.rb
+++ Formula/goprox.rb
@@ -1,8 +1,8 @@
 class Goprox < Formula
   desc "Missing GoPro workflow manager for zsh"
   homepage "https://github.com/fxstein/GoProX"
-  url "https://github.com/fxstein/GoProX/archive/refs/tags/v00.15.00.tar.gz"
-  sha256 "dd15806662862d390d600b364e87387569d585f5d5fc8c3b2dfec310d0dee63a"
+  url "https://github.com/fxstein/GoProX/archive/refs/tags/v00.16.00.tar.gz"
+  sha256 "a589ac0c9f1448b4ac0517391dbf2d9d15585efbce05fb46f86d76d29595fcf1"
   license "MIT"
 
   depends_on "exiftool"
