91aa45b788233c97d3ed25b75027a499207ae7e8
fxstein <fxstein@users.noreply.github.com>
2023-01-01 21:38:11 -0800

    Update goprox to v00.50.00

--- Formula/goprox.rb
+++ Formula/goprox.rb
@@ -1,8 +1,8 @@
 class Goprox < Formula
   desc "Missing GoPro workflow manager for zsh"
   homepage "https://github.com/fxstein/GoProX"
-  url "https://github.com/fxstein/GoProX/archive/refs/tags/v00.20.00.tar.gz"
-  sha256 "5c608e9041dd009697848b2ef55ee1fdf363bf421bd4791b1c4f69d798d1947a"
+  url "https://github.com/fxstein/GoProX/archive/refs/tags/v00.50.00.tar.gz"
+  sha256 "8ac8ca1eb8ee98b09fa7e10f0045f0b3203757d96c0b354b308042af5da1fadf"
   license "MIT"
 
   depends_on "exiftool"
@@ -14,5 +14,21 @@ class Goprox < Formula
     bin.install "firmware"
     bin.install "firmware.labs"
     man1.install "man/goprox.1"
+  end
+
+  install do
+    # Copy the com.goprox.plist file to the users LaunchAgents directory
+    cp "#{pkgshare}/launchctl/com.goprox.mount.plist" "~/Library/LaunchAgents"
+  
+    # Load the com.goprox.plist file and register it with launchd
+    # system "launchctl", "load", "~/Library/LaunchAgents/com.goprox.mount.plist"
+  end
+  
+  uninstall do
+    # Unload the com.goprox.plist file and remove it from launchd
+    # system "launchctl", "unload", "~/Library/LaunchAgents/com.goprox.mount.plist"
+  
+    # Remove the com.goprox.plist file from the LaunchAgents directory
+    rm "~/Library/LaunchAgents/com.goprox.mount.plist"
   end
 end
